d73df02cd3c8c9ff2b0ed00293e750703da3974b
swwayam <codingswayam24@gmail.com>
2023-06-16 23:26:00 +0530

    onetrade complete

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// import images as relative image path won't work with vite/vercel.
+ScrollReveal().reveal('.hero-image', {delay: 250});
+ScrollReveal().reveal('.hero-content-info', {delay: 250});
+ScrollReveal().reveal('.hero-content__customer', {delay: 250});
+ScrollReveal().reveal('.hero-content__review', {delay: 250});
+ScrollReveal().reveal('.about-us__button', {delay: 250});
+ScrollReveal().reveal('.sushi__title', {delay: 250});
+ScrollReveal().reveal('.sushi__description', {delay: 250});
+ScrollReveal().reveal('.popular-foods', {delay: 250});
+ScrollReveal().reveal('.trending', {delay: 250});
+ScrollReveal().reveal('.header__nav', {delay: 250});
